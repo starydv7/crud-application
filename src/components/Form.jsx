c26fe393f54db262232ad8c5c3aaba363674e6ae
starydv7 <pawanstar72926@gmail.com>
2022-10-12 00:06:30 +0530

    form ready

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,29 @@ const Form = () => {
     const [email, setEmail] = useState("");
     const [role, setRole] = useState("");
     const [full, setFull] = useState("");
-    
+     let handleSubmit = async (e) => {
+       e.preventDefault();
+       var DetailsObj = {
+         name: name,
+         mobile: mobile,
+         role: role,
+         full: full,
+         email: email,
+        
+       };
+       fetch("https://apipawan3.herokuapp.com/users", {
+         method: "POST",
+         headers: {
+           "content-Type": "application/json",
+         },
+         body: JSON.stringify(DetailsObj),
+       })
+         .then((res) => res.json())
+         .then((res) => {
+           console.log(res);
+         })
+         .catch((err) => {});
+     };
   return (
     <div>Form</div>
   )
