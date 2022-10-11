9990b644ef832fafc2f6879f7af1b21fa760d542
starydv7 <pawanstar72926@gmail.com>
2022-10-12 00:03:54 +0530

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Form = () => {
+    const [name, setName] = useState("");
+    const [mobile, setMobile] = useState("");
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
+    const [full, setFull] = useState("");
+    
   return (
     <div>Form</div>
   )
