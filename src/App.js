989b2f81bb06e37d3765a7a94aa82a4f3363cb38
starydv7 <pawanstar72926@gmail.com>
2022-10-12 00:00:59 +0530

    form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <br></br>
+      <Form/>
       <HomePage/>
     </div>
   );
