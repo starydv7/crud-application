dc84bc39784d4f6875168ce4b70ac17495c95fd7
starydv7 <pawanstar72926@gmail.com>
2022-10-12 00:28:59 +0530

    form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,30 +29,38 @@ const Form = () => {
      };
   return (
     <div className={styles.form}>
-      <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
+              <label>Role</label>
         <select value={role} onChange={(e) => setRole(e.target.value)}>
           <option value="/">Role</option>
           <option value="FrontEnd">Front_End</option>
           <option value="backend">Backend</option>
         </select>
+              <br></br>
+              <label>Name</label>
         <input
           type="text"
           value={name}
           placeholder="Name"
           onChange={(e) => setName(e.target.value)}
         />
+              <br></br>
+              <label>Contact Number</label>
         <input
           type="text"
           value={mobile}
           placeholder="Contact"
           onChange={(e) => setMobile(e.target.value)}
         />
+              <br></br>
+              <label>Email</label>
         <input
           type="text"
           value={email}
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
+        <br></br>
         <button type="submit">Submit</button>
         <div className="message">{message ? <p>{message}</p> : null}</div>
       </form>
