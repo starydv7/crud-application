a00f5b5019aedef00a205255dd47bca523efb7d8
starydv7 <pawanstar72926@gmail.com>
2022-10-12 00:17:29 +0530

    form ready

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ const Form = () => {
     const [mobile, setMobile] = useState("");
     const [email, setEmail] = useState("");
     const [role, setRole] = useState("");
-    const [full, setFull] = useState("");
+    const [message, setMessage] = useState("");
      let handleSubmit = async (e) => {
        e.preventDefault();
        var DetailsObj = {
@@ -29,8 +29,41 @@ const Form = () => {
          .catch((err) => {});
      };
   return (
-    <div>Form</div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select value={role} onChange={(e) => setRole(e.target.value)}>
+          <option value="/">Role</option>
+          <option value="FrontEnd">Front_End</option>
+          <option value="backend">Backend</option>
+          
+        </select>
+       
+        <input
+          type="text"
+          value={name}
+          placeholder="Adults"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={mobile}
+          placeholder="Capacity"
+          onChange={(e) => setMobile(e.target.value)}
+        />
+
+        <input
+          type="text"
+          value={email}
+          placeholder="Price"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
 }
 
 export default Form
