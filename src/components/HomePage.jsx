10342cc7a221cabd56c9b2d459fa2c878133eed5
starydv7 <pawanstar72926@gmail.com>
2022-10-12 13:21:27 +0530

    homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from "./homepage.module.css";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.homepage}>
+      <table>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Linkedin Id</td>
+            <td>Role</td>
+            <td>Conatct Number</td>
+            
+          </tr>
+        </tbody>
+   </table>
+    </div>
   )
 }
 
